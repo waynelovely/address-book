184a4b1c1cd60886e3afe6b399ac06d47bbc631b
root <root@li296-158.members.linode.com>
2013-06-19 21:18:12 -0400

    First commit for main program

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * A demo program in C for company X
+ *
+ * We wish to do a few things:
+ *
+ *   1. interact with the user on the command line
+ *   2. create/remove/update/delete entries from a data structure
+ *   3. persist that data structure to db tables
+ *   4. load the data structure into memory from db tables
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+
+
+int main( int argc, char * argv[] ) {
+
+  printf("hello world\n");
+
+}
